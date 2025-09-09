6f6d97ae9e140445c68778c9fd5c07b79dd262d6
Md Limon Islam <limon222019@gmail.com>
2025-09-09 14:24:10 +0600

    complete contact screen

--- lib/views/screen/Contact/contact_screen.dart
+++ lib/views/screen/Contact/contact_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ree_social_media_app/utils/app_colors.dart';
 import 'package:ree_social_media_app/views/base/bottom_menu..dart';
+import 'package:ree_social_media_app/views/base/custom_button.dart';
+import 'package:ree_social_media_app/views/base/custom_text_field.dart';
 
 class ContactScreen extends StatefulWidget {
   const ContactScreen({super.key});
@@ -9,9 +13,122 @@ class ContactScreen extends StatefulWidget {
 }
 
 class _ContactScreenState extends State<ContactScreen> {
+
+  final searchTextController = TextEditingController();
+  final List<Map<String, dynamic>> searchList = [
+    {
+      "name": "Mr. John",
+      "image":"assets/images/dummy.jpg", // image story
+      "invite": "assets/icons/message.svg",
+      "isInvite": false
+    },
+    {
+      "name": "Mr. John",
+      "image":"assets/images/dummy.jpg",
+      "invite": "assets/icons/message.svg",
+
+      "isInvite": true
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 24,),
+            Text("Contact List",
+            style: TextStyle(
+              color: AppColors.textColor,
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),),
+            SizedBox(height: 24,),
+            CustomTextField(controller: searchTextController,
+              borderColor: Colors.transparent,
+              suffixIcon: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SvgPicture.asset('assets/icons/search.svg'),
+              ),
+              hintText: 'Search here',),
+            Expanded(
+              child: ListView.separated(
+                itemBuilder: (context, index) {
+                  final item = searchList[index];
+                  return Row(
+                    children: [
+                      Container(
+                        height: 44,
+                        width: 44,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage(item['image']),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                      Text(
+                        "${item['name']}",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: AppColors.textColor,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      const Spacer(),
+                      item['isInvite'] == true
+                          ? SvgPicture.asset(item['invite'],
+                        color: Color(0xFF9CC198),)
+                          : Container(
+                        height: 38,
+                        width: 80,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(
+                            color: const Color(0xFFC4C3C3),
+                            width: 0.5,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color(0xFF002329).withValues(alpha: 0.5),
+                              spreadRadius: -1.25,
+                              blurRadius: 4,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Invite",
+                            style: TextStyle(
+                              color: Color(0xFF676565),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+                separatorBuilder: (_, _) => const SizedBox(height: 16),
+                itemCount: searchList.length,
+              ),
+            ),
+            CustomButton(
+                onTap: (){},
+                text: "Create Group")
+
+          ]
+        ),
+      ),
       bottomNavigationBar: BottomMenu(2),
     );
   }
